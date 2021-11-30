96fd1382cdf5f8b5f39b8789f133538ea5e39c6d
G7M7T1 <genlisense@gmail.com>
2021-11-30 07:56:27 -0500

    Newday Work
    Add second page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("change");
   menu.classList.toggle("change");
 });
+
+const video = document.querySelector(".video");
+const btn = document.querySelector(".buttons i");
+const bar = document.querySelector(".video-bar");
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = "far fa-pause-circle";
+  } else {
+    video.pause();
+    btn.className = "far fa-play-circle";
+  }
+};
+
+btn.addEventListener("click", () => {
+  playPause();
+});
+
+video.addEventListener("timeupdate", () => {
+  // console.log(video.currentTime, video.duration);
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%`;
+  if (video.ended) {
+    btn.classList = "far fa-play-circle";
+    bar.style.width = "0%";
+  }
+});
