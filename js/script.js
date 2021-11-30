5e91149aab001197b0efca99036f8858e9227521
G7M7T1 <genlisense@gmail.com>
2021-11-29 21:59:43 -0500

    New File

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// For Nav
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
