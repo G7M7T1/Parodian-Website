740ce72edbd3614cf25a3e7741e30cdc30766d00
G7M7T1 <genlisense@gmail.com>
2021-11-30 10:31:34 -0500

    1

--- js/script.js
+++ js/script.js
@@ -34,3 +34,17 @@ video.addEventListener("timeupdate", () => {
     bar.style.width = "0%";
   }
 });
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
