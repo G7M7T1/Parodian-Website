23d0aff443deb65287d6451d184ec1cf46777d07
G7M7T1 <genlisense@gmail.com>
2021-12-01 01:02:05 -0500

    New Date

--- js/script.js
+++ js/script.js
@@ -35,16 +35,16 @@ video.addEventListener("timeupdate", () => {
   }
 });
 
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 20,
-    stretch: 1,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 20,
+//     stretch: 1,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+// });
